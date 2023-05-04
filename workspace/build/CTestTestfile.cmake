a0f5d03cc340287786f08c38dd8a8497cd8b2fc4
Azur Causevic <azur.causevic@tum.de>
2023-05-04 11:47:36 +0200

    Tutorial 2

---
+++ workspace/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nao/workspace/src/tutorial_2
+# Build directory: /home/nao/workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ workspace/devel/_setup_util.py
@@ -0,0 +1,1 @@
+/home/nao/workspace/devel/.private/catkin_tools_prebuild/_setup_util.py
